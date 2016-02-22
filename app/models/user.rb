127003e212e507553a61601906102d7e2b3eecad
makitokezuka <kezuka@media-cf.co.jp>
2016-02-22 02:47:31 +0000

    modify error message for new@user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-    before_save { self.email = email.downcase }　#メールアドレスを小文字に変換。before_saveで保存する前にメソッド処理
-    validates :name, presence: true, length: {maximum:50} #　名前のバリデーション
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i　#メールアドレスの正規表現パターンの定義
+    before_save { self.email = email.downcase }
+    validates :name, presence: true, length: {maximum:50} 
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: {maximum:25}, format: {with: VALID_EMAIL_REGEX},uniqueness: {case_sensitive: false }
     has_secure_password
 end
