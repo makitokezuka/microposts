a08a547e060804bb16b531f28975ba2ead0de346
makitokezuka <kezuka@media-cf.co.jp>
2016-02-23 04:49:26 +0000

    add model Micropost table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219072021) do
+ActiveRecord::Schema.define(version: 20160223040722) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+  add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160219072021) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "test2"
+    t.string   "country"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
