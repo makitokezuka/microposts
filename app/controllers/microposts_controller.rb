bd1adc7d6ca6f7485130d8227e1dfd9cea2e15cf
makitokezuka <kezuka@media-cf.co.jp>
2016-02-23 06:26:21 +0000

    apply micro post index to user page

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -5,7 +5,7 @@ before_action :logged_in_user, only: [:create]
     def create
         @micropost = current_user.microposts.build(micropost_params)
         if @micropost.save
-            flash[:seccess] = "micropost created"
+            flash[:success] = "micropost created"
             redirect_to root_url
         else
             render 'static_pages/home'
