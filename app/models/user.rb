fd9dfbd3d52887df40b8b50f9599207894357cbc
makitokezuka <kezuka@media-cf.co.jp>
2016-02-23 14:18:33 +0000

    modify　error message in edit.html

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: {maximum:25}, format: {with: VALID_EMAIL_REGEX},uniqueness: {case_sensitive: false }
     has_secure_password
+    
     has_many :microposts
     
     has_many :following_relationships, class_name: "Relationship",
