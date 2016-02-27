dd909c459c7f9b4e6f84bb83c506bc4a8dd468eb
makitokezuka <kezuka@media-cf.co.jp>
2016-02-27 07:27:49 +0000

    apply kaminari

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,11 +2,10 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost = current_user.microposts.build
-      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc).page params[:page]
       #user.rbで定義したfeed_itemsがそのユーザのフォローしている人のマイクロポストを
       #全て取得している。includes(:user)の部分はつぶやきに含まれるユーザ情報を予め先読み（プリロード）する処理を行うために用いる
       @user = current_user
-      
     end
     
 
